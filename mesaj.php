98ed9cde32b668fbd4631265af745029144354f1
benexper <experins211@gmail.com>
2025-08-10 07:02:09 +0300

    Update mesaj.php

--- mesaj.php
+++ mesaj.php
@@ -1,24 +1,23 @@
+<?php
+echo "<b>🚀 PANEL UYGULAMAMIZ ÇIKTI!</b><br><br>";
 
-<b>🚀 PANEL UYGULAMAMIZ ÇIKTI!</b><br>
+echo "<b>UYGULAMA HAKKINDA</b><br>";
+echo "Artık bayilerinize veya istediğiniz kişiye panele erişim sağlamak için özel panel uygulamamızı kullanabilirsiniz!<br><br>";
 
-<b>UYGULAMA HAKKINDA</b><br>
-Artık bayilerinize veya istediğiniz kişiye panele erişim sağlamak için özel panel uygulamamızı kullanabilirsiniz!<br>
+echo "✔️ Kullanımı kolay modern arayüz<br>";
+echo "✔️ Hızlı ve güvenli erişim<br>";
+echo "✔️ Tüm panel özellikleri elinizin altında<br>";
+echo "✔️ Özel tasarım ve kolay yönetim<br><br>";
 
-✔️ Kullanımı kolay modern arayüz<br>
-✔️ Hızlı ve güvenli erişim<br>
-✔️ Tüm panel özellikleri elinizin altında<br>
-✔️ Özel tasarım ve kolay yönetim<br>
+echo "<b>UYGULAMAYI İNDİRMEK İÇİN</b><br><br>";
 
-<b>UYGULAMAYI İNDİRMEK İÇİN</b><br>
+echo "<b>PANEL İNDİRME LİNKİ V1</b><br>";
+echo "<a href='https://kuropanel.com/KEY PANEL @benexper.apk' target='_blank'>https://kuropanel.com/KEY PANEL @benexper.apk</a><br><br>";
 
-<b>PANEL İNDİRME LİNKİ V1</b><br>
-<a href='https://kuropanel.com/KEY PANEL @benexper.apk' target='_blank'>https://kuropanel.com/KEY PANEL @benexper.apk</a><br>
+echo "<b>PANEL İNDİRME LİNKİ V2</b><br>";
+echo "<a href='https://t.me/c/2303083891/30' target='_blank'>https://t.me/c/2303083891/30</a><br><br>";
 
-<b>PANEL İNDİRME LİNKİ V2</b><br>
-<a href='https://t.me/c/2303083891/30' target='_blank'>https://t.me/c/2303083891/30</a><br>
-
-<b>DESTEK VE YARDIM</b><br>
-Herhangi bir sorunuz için bize ulaşın:<br>
-<a href='https://t.me/Benexper' target='_blank'>https://t.me/Benexper</a><br>
-";
+echo "<b>DESTEK VE YARDIM</b><br>";
+echo "Herhangi bir sorunuz için bize ulaşın:<br>";
+echo "<a href='https://t.me/Benexper' target='_blank'>https://t.me/Benexper</a><br>";
 ?>
